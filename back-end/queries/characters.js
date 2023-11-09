13101de005e05dbba47153781e2e87ea1162a5af
Shanice <shanicegriffin@pursuit.org>
2023-11-08 21:13:27 -0500

    finished character queries

---
+++ back-end/queries/characters.js
@@ -0,0 +1,68 @@
+const db = require("../db/dbConfig.js");
+
+const getAllCharaters = async () => {
+  try {
+    const allCharaters = await db.any("SELECT * FROM characters");
+    return allCharaters;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getOneCharater = async (id) => {
+  try {
+    const oneCharacter = await db.one(
+      "SELECT * FROM characters WHERE id=$1",
+      id
+    );
+    return oneCharacter;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createCharater = async (character) => {
+  try {
+    const { name, bounty, crew, posterImage, hasPower } = character;
+    const createdCharacter = await db.one(
+      "INSERT INTO characters (name, bounty, crew, posterImage, hasPower) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [name, bounty, crew, posterImage, hasPower]
+    );
+    return createdCharacter;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteCharater = async (id) => {
+  try {
+    const deletedCharacter = await db.one(
+      "DELETE from characters WHEREid =$1 RETURNING *",
+      id
+    );
+    return deletedCharacter;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateCharater = async (id, character) => {
+  try {
+    const { name, bounty, crew, posterImage, hasPower } = character;
+    const updatedCharacter = await db.one(
+      "UPDATE character SET name=$1, bounty=$2, crew=$3, posterImage=$4, hasPower=$5 WHERE id=$6 RETURNING *",
+      [name, bounty, crew, posterImage, hasPower, id]
+    );
+    return updatedCharacter;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  getAllCharaters,
+  getOneCharater,
+  createCharater,
+  deleteCharater,
+  updateCharater,
+};
