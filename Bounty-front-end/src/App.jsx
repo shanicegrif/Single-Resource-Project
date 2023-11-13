aca366fe8871f033a7260dd4bb8ade01a60924e5
Shanice <shanicegriffin@pursuit.org>
2023-11-12 20:09:30 -0500

    started component setup

--- Bounty-front-end/src/App.jsx
+++ Bounty-front-end/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from "../Components/NavBar";
+import Index from "../Components/Index";
+import New from "../Components/New";
+import Show from "../Components/Show";
+import Update from "../Components/Update";
+// Example file: src/App.js
+import.meta.env.VITE_KEY;
+console.log(`${import.meta.env.VITE_BASE_URL}`);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Bounty Board</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/characters" element={<Index />} />
+            <Route path="/characters/new" element={<New />} />
+            <Route path="/characters/:id" element={<Show />} />
+            <Route path="/characters/:id/edit" element={<Update />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
