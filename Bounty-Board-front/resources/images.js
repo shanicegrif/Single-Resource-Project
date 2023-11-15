3d4b2ae0e74cb4d97828bf64072294c21a50af64
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-11-14 21:21:02 -0500

    Adds index file, image resources and multiple bug fixes w/ Shanice

---
+++ Bounty-Board-front/resources/images.js
@@ -0,0 +1,83 @@
+import apoo from "../resources/Apoo.png";
+import avalo from "../resources/Avalo.png";
+import basil from "../resources/Basil.png";
+import blackbeard from "../resources/Blackbeard.png";
+import chopper from "../resources/Chopper.png";
+import clown from "../resources/Clown.png";
+import compote from "../resources/Compote.png";
+import cracker from "../resources/Cracker.png";
+import daifuku from "../resources/Daifuku.png";
+import devon from "../resources/Devon.png";
+import diamante from "../resources/Diamante.png";
+import docQ from "../resources/DocQ.png";
+import doflamingo from "../resources/Doflamingo.png";
+import drake from "../resources/Drake.png";
+import franky from "../resources/Franky.png";
+import jack from "../resources/Jack.png";
+import jesus from "../resources/Jesus.png";
+import kaido from "../resources/Kaido.png";
+import katakuri from "../resources/Katakuri.png";
+import king from "../resources/King.png";
+import laffitte from "../resources/Laffitte.png";
+import laoG from "../resources/LaoG.png";
+import linlin from "../resources/Linlin.png";
+import machvise from "../resources/Machvise.png";
+import nami from "../resources/Nami.png";
+import oven from "../resources/Oven.png";
+import page from "../resources/Page.png";
+import perospero from "../resources/Perospero.png";
+import pica from "../resources/Pica.png";
+import pink from "../resources/Pink.png";
+import queen from "../resources/Queen.png";
+import robin from "../resources/Robin.png";
+import sanji from "../resources/Sanji.png";
+import shiryu from "../resources/Shiryu.png";
+import smoothie from "../resources/Smoothie.png";
+import trebol from "../resources/Trebol.png";
+import usopp from "../resources/Usopp.png";
+import vanAugur from "../resources/VanAugur.png";
+import zoro from "../resources/Zoro.png";
+
+const images = {
+    apoo: apoo,
+    avalo: avalo,
+    basil: basil,
+    blackbeard: blackbeard,
+    chopper: chopper,
+    clown: clown,
+    compote: compote,
+    cracker: cracker,
+    daifuku: daifuku,
+    devon: devon,
+    diamante: diamante,
+    docQ: docQ,
+    doflamingo: doflamingo,
+    drake: drake,
+    franky: franky,
+    jack: jack,
+    jesus: jesus,
+    kaido: kaido,
+    katakuri: katakuri,
+    king: king,
+    laffitte: laffitte,
+    laoG: laoG,
+    linlin: linlin,
+    machvise: machvise,
+    nami: nami,
+    oven: oven,
+    page: page,
+    perospero: perospero,
+    pica: pica,
+    pink: pink,
+    queen: queen,
+    robin: robin,
+    sanji: sanji,
+    shiryu: shiryu,
+    smoothie: smoothie,
+    trebol: trebol,
+    usopp: usopp,
+    vanAugur: vanAugur,
+    zoro: zoro,
+};
+
+export default images;
