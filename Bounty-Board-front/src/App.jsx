fca57a60973b19c00cb748baaa781ac72775dd9e
Shanice <shanicegriffin@pursuit.org>
2023-11-14 22:05:06 -0500

    routes created

--- Bounty-Board-front/src/App.jsx
+++ Bounty-Board-front/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css'
-import BountyPoster from '../Components/BountyPoster.jsx';
-import Index from '../Components/Index';
-import Nav from '../Components/NavBar';
+import "./App.css";
+import BountyPoster from "../Components/BountyPoster.jsx";
+import Index from "../Components/Index";
+import Nav from "../Components/NavBar";
+import Home from "../Components/Home.jsx";
+import New from "../Components/New";
+import Show from "../Components/Show";
+import Update from "../Components/Update";
 
 function App() {
   return (
     <>
-    <div className ="Home">
-      <Router>
-        <Nav/>
-        <Routes>
-          <Route path="/" element={<Index/>}/>
-        </Routes>
-      </Router>
-    </div>
+      <div>
+        <Router>
+          <Nav />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/characters" element={<Index />} />
+              <Route path="/characters/new" element={<New />} />
+              <Route path="/characters/:id" element={<Show />} />
+              <Route path="/characters/:id/edit" element={<Update />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
